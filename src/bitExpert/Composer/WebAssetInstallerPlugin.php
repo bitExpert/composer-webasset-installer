97a327d33b1be5620bc9d0bd66a7d37f0ad7bd35
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2013-10-11 15:40:44 +0200

    Fixed docblock formatting.

--- src/bitExpert/Composer/WebAssetInstallerPlugin.php
+++ src/bitExpert/Composer/WebAssetInstallerPlugin.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * This file is part of the Web Asset Installer Plugin.
-*
-* (c) bitExpert AG
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ *
+ * (c) bitExpert AG
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 
 namespace bitExpert\Composer;
